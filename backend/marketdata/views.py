7414277f3f9f08a0a4b322951b993e44308f78e5
hbiegacz <biegacz.hanna@gmail.com>
2025-11-05 17:26:44 +0100

    added test views

--- backend/marketdata/views.py
+++ backend/marketdata/views.py
@@ -1,9 +1,50 @@
-from django.shortcuts import render
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# binance api view
+from .services.BinanceAPIService import BinanceAPIService
+from .services.FredAPIService import FredAPIService
+from .services.CoinmetricsAPIService import CoinmetricsAPIService
+from .services.StooqAPIService import StooqAPIService
 
-# fred api view
 
-# stooq view
+class BinanceTestView(APIView):
+    def get(self, request, *args, **kwargs):
+        service = BinanceAPIService()
+        try:
+            data = service.get_btc_price()
+            return Response(data, status=status.HTTP_200_OK)
+        except requests.RequestException as e:
+            return Response({"error": "Could not fetch price from Binance API", "details": str(e)},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-# coinmetrics view
+class FredTestView(APIView):
+    def get(self, request, *args, **kwargs):
+        service = FredAPIService()
+        try:
+            data = service.get_basic_economic_data()
+            return Response(data, status=status.HTTP_200_OK)
+        except requests.RequestException as e:
+             return Response({"error": "Could not fetch data from Stooq API", "details": str(e)},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        
+class StooqTestView(APIView):
+    def get(self, request, *args, **kwargs):
+        service = StooqAPIService()
+        try:
+            data = service.get_sp500_data()
+            return Response(data, status=status.HTTP_200_OK)
+        except requests.RequestException as e:
+            return Response({"error": "Could not fetch data from Stooq API", "details": str(e)},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+class CoinmetricsTestView(APIView):
+    def get(self, request, *args, **kwargs):
+        service = CoinmetricsAPIService()
+        try:
+            data = service.get_reference_rate()
+            return Response(data, status=status.HTTP_200_OK)
+        except requests.RequestException as e:
+            return Response({"error": "Could not fetch data from Coinmetrics API", "details": str(e)},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
