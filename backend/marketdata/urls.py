b9d37dcd68b0cd3861e44811c72a28dfff996cba
hbiegacz <biegacz.hanna@gmail.com>
2025-11-05 12:47:00 +0100

    created new django app marketdata

---
+++ backend/marketdata/urls.py
@@ -0,0 +1,6 @@
+# marketdata/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+]
---
+++ backend/marketdata/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# binance api view
+
+# fred api view
+
+# stooq view
+
+# coinmetrics view
