0b11660063418e850187847c5b30833c89a1520b
hbiegacz <biegacz.hanna@gmail.com>
2025-11-05 17:27:06 +0100

    added urls for each view

--- backend/marketdata/urls.py
+++ backend/marketdata/urls.py
@@ -1,6 +1,9 @@
-# marketdata/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('binance-test/', views.BinanceTestView.as_view(), name='binance-test'),
+    path('fred-test/', views.FredTestView.as_view(), name='fred-test'),
+    path('stooq-test/', views.StooqTestView.as_view(), name='stooq-test'),
+    path('coinmetrics-test/', views.CoinmetricsTestView.as_view(), name='coinmetrics-test'),   
 ]
