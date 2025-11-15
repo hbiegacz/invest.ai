bfa285071188f8d4e69f4d97d0b32868d2b33faf
Adrian Garbowski <adrianq.8949@gmail.com>
2025-11-15 07:21:46 +0100

    Finished Binance API integration with Historical Endpoint

--- backend/marketdata/urls.py
+++ backend/marketdata/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('binance-test/', views.BinanceTestView.as_view(), name='binance-test'),
     path('fred-test/', views.FredTestView.as_view(), name='fred-test'),
     path('stooq-test/', views.StooqTestView.as_view(), name='stooq-test'),
-    path('coinmetrics-test/', views.CoinmetricsTestView.as_view(), name='coinmetrics-test'),   
+    path('coinmetrics-test/', views.CoinmetricsTestView.as_view(), name='coinmetrics-test'),
+    path('historical-data/', views.HistoricalDataView.as_view(), name='historical-data'),
 ]
